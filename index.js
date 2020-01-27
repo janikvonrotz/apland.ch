341ecd1682f28c9ac55b4d9bd660066d36692d2c
Janik von Rotz <login@janikvonrotz.ch>
2020-01-27 18:40:59 +0100

    add function to index

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ Reveal.initialize({
   progress: false
 })
 
+function openNav () {
+  document.getElementById('navigation').style.height = '100%'
+}
+
+function closeNav () {
+  document.getElementById('navigation').style.height = '0%'
+}
